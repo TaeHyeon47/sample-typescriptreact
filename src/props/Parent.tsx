4caea702855a1d0efb328da8f52ae432ae3404b5
Tae Hyeon Jo <hillskuti@naver.com>
2023-04-11 20:27:41 +0900

    Props 모델 정의

--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -1,7 +1,8 @@
 import { Child } from './Child';
+// import { ChildAsFC } from './Child';
 
 const Parent = () => {
-  return <Child color='red' />;
+  return <Child color='red' onClick={() => console.log('Clicked')}></Child>;
 };
 
 export default Parent;
