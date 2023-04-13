05f8c7db078b114e1f11d191f95de9c25cb1a918
Tae Hyeon Jo <hillskuti@naver.com>
2023-04-13 22:11:43 +0900

    타입스크립트 ref, 드래그 등

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import EventComponent from './event/EventComponent';
+import UserSearch from './refs/UserSearch';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <UserSearch />
   </React.StrictMode>
 );
 
